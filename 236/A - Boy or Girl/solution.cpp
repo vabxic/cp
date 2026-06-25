33412574de2402440e5af9769f9dd3d0832fc8a5
Vaibhav Singh <vaibhavsingh4805@gmail.com>
2026-06-25 16:53:04 +0530

    Add Boy or Girl [A] from Codeforces

---
+++ 236/A - Boy or Girl/solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    string str;
+    cin >>str;
+    set<char>s(str.begin(),str.end());
+    int n = s.size();
+    if (n%2==0){
+        cout<<"CHAT WITH HER!";
+    }
+    else{
+        cout<<"IGNORE HIM!";
+    }
+    return 0;
+}
