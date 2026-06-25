1420989314c562bab833c7ceee9cc117fc90ae0f
Vaibhav Singh <vaibhavsingh4805@gmail.com>
2026-06-25 16:53:24 +0530

    Add In Search of an Easy Problem [A] from Codeforces

---
+++ 1030/A - In Search of an Easy Problem/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int n;
+    cin >> n;
+ 
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        if (x == 1) {
+            cout << "HARD";
+            return 0;
+        }
+    }
+ 
+    cout << "EASY";
+    return 0;
+}
