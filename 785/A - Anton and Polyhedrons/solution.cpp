c971c963ac6ce6e6fbc65bae88ef061820e67bd0
Vaibhav Singh <vaibhavsingh4805@gmail.com>
2026-06-29 18:14:45 +0530

    Add Anton and Polyhedrons [A] from Codeforces

---
+++ 785/A - Anton and Polyhedrons/solution.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2")
+static const int _ = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+const int INF = 1e9 + 7;
+const ll LINF = 1e18 + 7;
+const ll MOD = 1e9 + 7;
+int main(){
+    int n;
+    cin >> n;
+    unordered_map<string,int>poly = {{"Tetrahedron",4},{"Cube",6},{"Octahedron",8},{"Dodecahedron",12},{"Icosahedron",20}};
+    int result = 0;
+    string s;
+    for (int i = 0; i < n; ++i){
+        cin >> s;
+        result += poly[s];
+    }
+    cout << result;
+    return 0;
+}
