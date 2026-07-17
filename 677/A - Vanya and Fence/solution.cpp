ad21223e3169e1cf3bf449b8f6dfa60150b6ff27
Vaibhav Singh <vaibhavsingh4805@gmail.com>
2026-07-17 11:41:16 +0530

    Add Vanya and Fence [A] from Codeforces

---
+++ 677/A - Vanya and Fence/solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int n, h;
+    cin >> n >> h;
+ 
+    int width = 0;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        if (x > h)
+            width += 2;
+        else
+            width += 1;
+    }
+ 
+    cout << width;
+    return 0;
+}
